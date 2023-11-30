f23b2609e803ce8afcd7a9d8b9353f204942f2f7
clarayuan <c_w_yuan@hotmail.com>
2023-11-30 14:19:49 -0500

    got it work finally -- pass the test on leetcode BFD

--- src/main/java/com/example/demo/map/StockSpanner.java
+++ src/main/java/com/example/demo/map/StockSpanner.java
@@ -1,5 +1,7 @@
 package com.example.demo.map;
 
+import org.hibernate.boot.model.source.internal.hbm.AttributesHelper;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -9,10 +11,15 @@ import java.util.stream.Collectors;
  * should use a stack to pop things off the list. it is quite better than this.
  * will have to work on it next. will get rid of most of the code.
  * second problem: when the input is crazy, with a lot of items, a lot of duplicates etc
- * this will time limit
+ * this will time limit.
+ * finally got it working with a simple trick of keeping min and max.
  */
 public class StockSpanner {
     Deque<Integer> stack = new LinkedList<>();
+
+    // keep a note of min and max of the whole thing, at least we can speed up a few edge cases
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
 
     public StockSpanner() {
 
@@ -21,27 +28,40 @@ public class StockSpanner {
     public int next(int price) {
         int retval = 1; // at least one
         int space = 0;
-        Deque<Integer> storestack = new LinkedList<>();
-        while (!stack.isEmpty()) {
 
-            int item = stack.pop();
-            if (item > price) {
-                // the end of the search
+        // there is no need to compare if it is larger or equal to max
+        if (price >= max) {
+            // space is the size of the entire stack
+            space = stack.size();
+        } else if (price < min) {
+            space = 0;
+        } else {
+            Deque<Integer> storestack = new LinkedList<>();
+            while (!stack.isEmpty()) {
+
+                int item = stack.pop();
+                if (item > price) {
+                    // the end of the search
+                    storestack.push(item);
+                    break;
+                } else {
+                    space++;
+                }
                 storestack.push(item);
-                break;
-            } else {
-                space++;
             }
-            storestack.push(item);
-        }
 
-        // put back the items
-        while (!storestack.isEmpty()) {
-            stack.push(storestack.pop());
+            // put back the items
+            while (!storestack.isEmpty()) {
+                stack.push(storestack.pop());
+            }
         }
 
         // push the price on it
         stack.push(price);
+        if (price < min)
+            min = price;
+        if (price > max)
+            max = price;
         return retval + space;
     }
 
