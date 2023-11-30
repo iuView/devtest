4f52ddf72e44d7e4f9b46b883733bfa21e7dde58
clarayuan <c_w_yuan@hotmail.com>
2023-11-30 09:57:25 -0500

    use stacks instead of tree for StockSpanner

--- src/main/java/com/example/demo/map/StockSpanner.java
+++ src/main/java/com/example/demo/map/StockSpanner.java
@@ -5,13 +5,12 @@ import java.util.stream.Collectors;
 
 /**
  * <a href="https://leetcode.com/problems/online-stock-span/">leetcode</a>
+ * looks like the TreeMap approach is not the best,
+ * should use a stack to pop things off the list. it is quite better than this.
+ * will have to work on it next. will get rid of most of the code
  */
 public class StockSpanner {
-    int currentposition = 0;
-
-    // need a hashmap to remember when the last time certain price occurred
-    // also it should sort the map key, so the search time is log(n) time
-    TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
+    Deque<Integer> stack = new LinkedList<>();
 
     public StockSpanner() {
 
@@ -19,58 +18,29 @@ public class StockSpanner {
 
     public int next(int price) {
         int retval = 1; // at least one
-        // now find:
-        int newprice = price + 1;
-        int space = Integer.MIN_VALUE;
-        Map<Integer, TreeSet<Integer>> submap = map.headMap(newprice);
-        Map<Integer, TreeSet<Integer>> tailmap = map.tailMap(newprice);
-        // need to make sure the values are continuous
-        if (!submap.isEmpty()) {
-            // need to combine all the numbers in set:
+        int space = 0;
+        Deque<Integer> storestack = new LinkedList<>();
+        while (!stack.isEmpty()) {
 
-            // todo: if the set is TreeSet then maybe we don't need to sort it like this:
-//            Comparator<? super Integer> comparator = new Comparator<Integer>() {
-//                @Override
-//                public int compare(Integer o1, Integer o2) {
-//                    return o1.compareTo(o2);
-//                }
-//            };
-            List<Integer> allvals = submap.values().stream().flatMap(TreeSet::stream).collect(Collectors.toList());
-            // need to find the continues ones, starting from the least index
-
-            int minindex = allvals.stream().sorted().findFirst().get();
-
-            if (tailmap.isEmpty()) {
-                // all are smaller, so no big deal
-                space = currentposition - minindex;
+            int item = stack.pop();
+            if (item > price) {
+                // the end of the search
+                storestack.push(item);
+                break;
             } else {
-                int i = currentposition - 1;
-                for (; i >= minindex; i--) {
-                    if (allvals.contains(i)) {
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-                space = currentposition - 1 - i;
+                space++;
             }
+            storestack.push(item);
         }
 
-        if (space >= 1)
-            retval += space;
+        // put back the items
+        while (!storestack.isEmpty()) {
+            stack.push(storestack.pop());
+        }
 
-        // but need to exclude the position that is larger than price
-        // tail map and head map may give some clues
-
-        // adding this data point:
-        TreeSet<Integer> indexes = map.get(price);
-        if (indexes == null)
-            indexes = new TreeSet<>();
-        indexes.add(currentposition);
-        map.put(price, indexes); // the index of it
-        currentposition++;
-        // end adding this data point
-
-        return retval;
+        // push the price on it
+        stack.push(price);
+        return retval + space;
     }
+
 }
--- src/test/java/com/example/demo/map/StockSpannerTest.java
+++ src/test/java/com/example/demo/map/StockSpannerTest.java
@@ -2,36 +2,9 @@ package com.example.demo.map;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.TreeMap;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StockSpannerTest {
-
-
-    @Test
-    void next() {
-        StockSpanner stockSpanner = new StockSpanner();
-        int result = stockSpanner.next(100); // return 1
-        assertThat(result).isEqualTo(1);
-        result = stockSpanner.next(80);  // return 1
-        assertThat(result).isEqualTo(1);
-        result = stockSpanner.next(60);  // return 1
-        assertThat(result).isEqualTo(1);
-
-        result = stockSpanner.next(70);  // return 2
-        assertThat(result).isEqualTo(2);
-
-        result = stockSpanner.next(60);  // return 1
-        assertThat(result).isEqualTo(1);
-
-        result = stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
-        assertThat(result).isEqualTo(4);
-
-        result = stockSpanner.next(85);  // return 6
-        assertThat(result).isEqualTo(6);
-
-    }
 
     @Test
     void next2() {
@@ -116,6 +89,30 @@ class StockSpannerTest {
 
         result = stockSpanner.next(70);
         assertThat(result).isEqualTo(7);
+
+    }
+
+    @Test
+    void next() {
+        StockSpanner stockSpanner = new StockSpanner();
+        int result = stockSpanner.next(100); // return 1
+        assertThat(result).isEqualTo(1);
+        result = stockSpanner.next(80);  // return 1
+        assertThat(result).isEqualTo(1);
+        result = stockSpanner.next(60);  // return 1
+        assertThat(result).isEqualTo(1);
+
+        result = stockSpanner.next(70);  // return 2
+        assertThat(result).isEqualTo(2);
+
+        result = stockSpanner.next(60);  // return 1
+        assertThat(result).isEqualTo(1);
+
+        result = stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+        assertThat(result).isEqualTo(4);
+
+        result = stockSpanner.next(85);  // return 6
+        assertThat(result).isEqualTo(6);
 
     }
 }
